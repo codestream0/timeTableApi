022522ee6ae7e30336207bf836c2d18d8f99a971
Bin-Abdoul <usamaabdulhakeem2006@gmail.com>
2025-07-01 12:01:25 +0100

    added role endpoint

--- src/variables.ts
+++ src/variables.ts
@@ -2,5 +2,5 @@ require("dotenv").config();
 
 export const accessSecret = process.env.ACCESS_TOKEN_SECRET || "asdfghfgh";
 export const mongodbUri =
-  process.env.MONGODB_URI || "mongodb://localhost:27017/";
+  process.env.MONGODB_URI || "mongodb+srv://abdoulbin38:<db_password>@cluster0.befyo9z.mongodb.net/";
 export const refreshSecret = process.env.REFRESH_TOKEN_SECRET || "sdfghghj";
