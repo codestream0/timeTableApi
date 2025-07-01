47d14541d1800fe97e10d1e331f8a50b030eaeec
Bin-Abdoul <usamaabdulhakeem2006@gmail.com>
2025-07-01 15:36:18 +0100

    bug fixes

--- src/variables.ts
+++ src/variables.ts
@@ -2,5 +2,5 @@ require("dotenv").config();
 
 export const accessSecret = process.env.ACCESS_TOKEN_SECRET || "asdfghfgh";
 export const mongodbUri =
-  process.env.MONGODB_URI || "mongodb+srv://abdoulbin38:<db_password>@cluster0.befyo9z.mongodb.net/";
+  process.env.MONGODB_URI || "mongodb+srv://abdoulbin38:w0bsa0IXB7mk2xcH@cluster0.5n02s7c.mongodb.net/timetabledb?retryWrites=true&w=majority&authSource=admin";
 export const refreshSecret = process.env.REFRESH_TOKEN_SECRET || "sdfghghj";
